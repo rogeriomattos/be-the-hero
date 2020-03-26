0f46fb12b185033f831e2c6b1f582772b94a9ea6
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-03-26 15:29:10 -0300

    Frontend do App

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -9,6 +9,7 @@ module.exports = {
     },
 
     async create(request, response) {
+        console.log(request.body);
         const { name, email, whatsapp, city, uf } = request.body;
     
         const id = crypto.randomBytes(4).toString("HEX");
