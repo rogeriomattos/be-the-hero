82544fc688072f8d60e39dfa124c8ce968685af2
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-03-30 12:08:29 -0300

    correção de contato

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -28,7 +28,7 @@ export default function Detail() {
     }
 
     function sendWhatsapp(){
-        Linking.openURL(`whatsapp://send?phone=5511954980848&text=${message}`)
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
     }
 
     return(
